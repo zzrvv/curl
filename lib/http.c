ef9cef98b969bbd8a27b7295e3e01f53cbd61933
Daniel Stenberg <daniel@haxx.se>
2020-12-11 10:21:28 +0100

    http: don't put the "reason" in error messages
    Updated test 24 accordingly. It's just not worth the trouble and it got
    a little complicated to do it for Hyper.

--- lib/http.c
+++ lib/http.c
@@ -3107,37 +3107,6 @@ checkprotoprefix(struct Curl_easy *data, struct connectdata *conn,
 static void print_http_error(struct Curl_easy *data)
 {
   struct SingleRequest *k = &data->req;
-  char *beg = Curl_dyn_ptr(&data->state.headerb);
-
-  /* make sure that data->req.p points to the HTTP status line */
-  if(!strncmp(beg, "HTTP", 4)) {
-
-    /* skip to HTTP status code */
-    beg = strchr(beg, ' ');
-    if(beg && *++beg) {
-
-      /* find trailing CR */
-      char end_char = '\r';
-      char *end = strchr(beg, end_char);
-      if(!end) {
-        /* try to find LF (workaround for non-compliant HTTP servers) */
-        end_char = '\n';
-        end = strchr(beg, end_char);
-      }
-
-      if(end) {
-        /* temporarily replace CR or LF by NUL and print the error message */
-        *end = '\0';
-        failf(data, "The requested URL returned error: %s", beg);
-
-        /* restore the previously replaced CR or LF */
-        *end = end_char;
-        return;
-      }
-    }
-  }
-
-  /* fall-back to printing the HTTP status code only */
   failf(data, "The requested URL returned error: %d", k->httpcode);
 }
 
